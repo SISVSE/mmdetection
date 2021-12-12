eaaa1e84abf3bc61e70a879ae146de1e54c910f0
Jihun Yoon <oshot1385@gmail.com>
2021-12-12 01:50:06 +0000

    init

--- demo/image_demo.py
+++ demo/image_demo.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import asyncio
 from argparse import ArgumentParser
+from os import path as osp
 
 from mmdet.apis import (async_inference_detector, inference_detector,
                         init_detector, show_result_pyplot)
@@ -15,10 +16,6 @@ def parse_args():
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
         '--score-thr', type=float, default=0.3, help='bbox score threshold')
-    parser.add_argument(
-        '--async-test',
-        action='store_true',
-        help='whether to set async options for async inference.')
     args = parser.parse_args()
     return args
 
@@ -29,22 +26,14 @@ def main(args):
     # test a single image
     result = inference_detector(model, args.img)
     # show the results
-    show_result_pyplot(model, args.img, result, score_thr=args.score_thr)
-
-
-async def async_main(args):
-    # build the model from a config file and a checkpoint file
-    model = init_detector(args.config, args.checkpoint, device=args.device)
-    # test a single image
-    tasks = asyncio.create_task(async_inference_detector(model, args.img))
-    result = await asyncio.gather(tasks)
-    # show the results
-    show_result_pyplot(model, args.img, result[0], score_thr=args.score_thr)
+    model.show_result(
+        args.img,
+        result,
+        out_file='_'.join([args.img.split('.jpg')[0], 'result.jpg']),
+        score_thr=args.score_thr)
+    print('_'.join([args.img.split('.jpg')[0], 'result.jpg']))
 
 
 if __name__ == '__main__':
     args = parse_args()
-    if args.async_test:
-        asyncio.run(async_main(args))
-    else:
-        main(args)
+    main(args)
